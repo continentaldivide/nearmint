cc4272b5a02355c09fe24c5600eca9ebc052b64e
Andrew <andrew@andrewsmith.org>
2023-04-10 22:06:19 -0400

    Implement adding comics to collection

--- controllers/comics.js
+++ controllers/comics.js
@@ -21,13 +21,34 @@ router.get("/search", async (req, res) => {
     let url = `https://gateway.marvel.com:443/v1/public/comics?titleStartsWith=${req.query.series}&orderBy=-focDate&ts=${newTime}&apikey=${process.env.PUB_KEY}&hash=${newHash}`;
     const response = await fetch(url);
     const responseJson = await response.json();
-    await res.locals.user.createComic({
-      marvel_id: responseJson.data.results[0].id,
-    });
+    // await res.locals.user.createComic({
+    //   marvel_id: responseJson.data.results[0].id,
+    // });
     res.render("comics/search", {
       series: req.query.series,
       comics: responseJson.data.results,
     });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    if (!res.locals.user) {
+      res.redirect("/users/login");
+    } else {
+      await res.locals.user.createComic({
+        marvel_id: req.body.id,
+        title: req.body.title,
+        series: req.body.series,
+        issue_number: req.body.issue_number,
+        thumbnail_url: req.body.thumbnail_url,
+        owned: req.body.owned || null,
+        wishlist: req.body.wishlist || null,
+      });
+      res.status(204).send();
+    }
   } catch (error) {
     console.log(error);
   }
