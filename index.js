7f8327406effd5402a60d1efc925accfa48d3fd6
Andrew <andrew@andrewsmith.org>
2023-04-11 17:29:51 -0400

    Implement collection page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cryptoJs = require("crypto-js");
 const db = require("./models");
+const methodOverride = require("method-override");
 
 // app config
 const app = express();
@@ -14,6 +15,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 app.use((req, res, next) => {
   console.log(`[${new Date().toLocaleString()}]: ${req.method} ${req.url}`);
   next();
